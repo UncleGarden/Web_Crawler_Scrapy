d7b1ca844244a7c72d7a8156f2f1f1ba728b885b
Jiateng Wang <wjt1223@gmail.com>
2016-02-24 23:35:01 -0800

    test to spider leetcode

--- Leetcode_Scrapy/leetcode_scrapy.py
+++ Leetcode_Scrapy/leetcode_scrapy.py
@@ -1,3 +1,19 @@
 import scrapy
 
-# Do something here
+class LeetcodeSpider(scrapy.Spider):
+    name = 'leetcode'
+    start_urls = ['https://leetcode.com/problemset/algorithms']
+
+    def parse(self, response):
+        for href in response.css('.row tr td a::attr(href)'):
+            full_url = response.urljoin(href.extract())
+            yield scrapy.Request(full_url, callback=self.parse_question)
+
+    def parse_question(self, response):
+        yield {
+            #'title': response.css('h1 a::text').extract()[0],
+            #'votes': response.css('.question .vote-count-post::text').extract()[0],
+            #'body': response.css('.question .post-text').extract()[0],
+            #'tags': response.css('.question .post-tag::text').extract(),
+            'link': response.url,
+        }
